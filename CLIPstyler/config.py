45a5a0a7915c90397e9e39296d84b90c917040c0
SShowbiz <hwkim408@snu.ac.kr>
2022-11-26 14:00:05 +0900

    feat: add several arguments on config

--- CLIPstyler/config.py
+++ CLIPstyler/config.py
@@ -14,6 +14,7 @@ def read_arguments():
 def add_all_arguments(parser):
     parser.add_argument("--content_path", type=str, default="./face.jpg", help="Image resolution")
     parser.add_argument("--mask_path", type=str, default="./background.jpg", help="Image resolution")
+    parser.add_argument("--output_path", type=str, default="output_images", help="output image path")
     parser.add_argument("--text", type=str, default="Fire", help="Image resolution")
     parser.add_argument("--lambda_tv", type=float, default=2e-3, help="total variation loss parameter")
     parser.add_argument("--lambda_patch", type=float, default=9000, help="PatchCLIP loss parameter")
@@ -24,8 +25,9 @@ def add_all_arguments(parser):
     parser.add_argument("--img_width", type=int, default=472, help="size of images")
     parser.add_argument("--img_height", type=int, default=680, help="size of images")
     parser.add_argument("--max_step", type=int, default=200, help="Number of domains")
-    parser.add_argument("--lr", type=float, default=5e-4, help="Number of domains")
+    parser.add_argument("--lr_stylize", type=float, default=5e-4, help="Number of domains")
     parser.add_argument("--thresh", type=float, default=0.7, help="Number of domains")
+    parser.add_argument("--num_output", type=int, default=1, help="Number of output image")
 
     return parser
 
