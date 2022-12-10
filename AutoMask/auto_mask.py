c579b5aef636ccd2df871a34bf92ba2b8213e827
SShowbiz <hwkim408@snu.ac.kr>
2022-12-11 02:03:52 +0900

    fix: change os.system to synchronous alternative

--- AutoMask/auto_mask.py
+++ AutoMask/auto_mask.py
@@ -1,21 +1,25 @@
-import os
 import cv2
-import numpy as np
-from PIL import Image
+from subprocess import Popen
 
 def auto_mask(filedir):
     # Remove Background
     last_slash = filedir.rindex('/')
     filename = filedir[last_slash+1:]
     nobg_filedir = filedir[:last_slash+1] + 'nobg_' + filename
-    os.system('backgroundremover -i ' + filedir + ' -o ' + nobg_filedir)
+
+    Popen(['backgroundremover', '-i', filedir, '-o', nobg_filedir]).wait()
     
     # Get mask
     input_image = cv2.imread(filedir) 
     nobg_image = cv2.imread(nobg_filedir)
+
     pre_mask = cv2.cvtColor(input_image - nobg_image, cv2.COLOR_BGR2GRAY)
 
     _, mask = cv2.threshold(pre_mask, 5, 255, cv2.THRESH_BINARY)
-    cv2.imwrite(filedir[:last_slash+1] + 'masked_' + filename, mask)
+
+    mask_file_name = filedir[:last_slash+1] + 'masked_' + filename
+    cv2.imwrite(mask_file_name, mask)
     
-    os.system('rm ' + nobg_filedir)
+    Popen(['rm', nobg_filedir]).wait()
+
+    return mask_file_name
